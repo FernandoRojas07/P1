48ddfa19368af2b15ec2e5ad733732bb450e3ce4
Mariooo2 <marioa_bros@hotmail.com>
2015-09-30 18:56:12 -0500

    Update DireccionP.java

--- DireccionP.java
+++ DireccionP.java
@@ -4,10 +4,9 @@ class DireccionP{
    {
       LeeDelegacion();
       }
-   public void LeeDelegacion()
-   {
-      delegacion=Teclado.LeeCadena("Delegacion: ");
-   }
+   public void LeeDelegacion(){
+delegacion=Teclado.LeeEntero("En que delegacion viven: \n1.-Coyoacan\n2.-Gustavo A. Madero\n3.-Iztapalapa\n4.-Álvaro Obregón\n5.-Tlalpan\n6.-Otra\n");
+}
    
    public void Listar()
    {
