d439d5558b23e0229615194a93e979439422e3fb
Mariooo2 <marioa_bros@hotmail.com>
2015-09-30 19:12:24 -0500

    Update Inicio.java

--- Inicio.java
+++ Inicio.java
@@ -1,50 +1,20 @@
-class Inicio
-{
-   static CasaP cas = new CasaP();
-   static Estadisticas estadis = new Estadisticas();
-   public static int listaMenu()
-   {
-      int op;
-      do 
-      {
-         op =Teclado.LeeEntero("1)Iniciar censo.\n2)Consultar estadisticas.\n3)Salir.\n");
-         if (op>3 || op<1)
-            System.out.println("Error, opción no valida.");
-      }
-      while(op>7);
-      return op; 
-   }
-   public static void main(String []args)
-   {
-      int opc;
-      do 
-      {
-         opc = listaMenu();
-         switch(opc)
-         {
-            case 1: cas.CasasCap();
-               break;
-            case 2: estadis.listaMenu();
-               break;
-         }
-      }
-      while(opc>7);
-   }
-}
-/*public static void main(String []args)   {
+class InicioP {
+ 
+   public static void main(String []args)   {
       int opcion,indice;
+      CasaP casa = new CasaP();
       Menu menu=new Menu();
-      Estadisticas e;
+      EstadisticasP e=new EstadisticasP();
       do {
-         opcion=menu.Menu("1)Iniciar censo.\ln2)Consultar estadisticas.\ln3)Salir.\ln",3);
+         opcion=menu.Menu("1)Iniciar censo.\n2)Consultar estadisticas.\n3)Salir.\n",3);
          switch(opcion) {
             case 1:System.out.println("Iniciando...");
-                   indice=Teclado.Lee("Dame el numero total de casas para el censo: ");
-                   per=new PersonaP[indice];
+                   //indice=Teclado.LeeEntero("Dame el numero total de casas para el censo: ");
+                   casa.CasasCap();
                   break;
-            case 2:e.CalculaEstadisticas()
+            case 2:e.CalculaEstadisticas();
                   break;
                   }
       } while(opcion!=3);
-}*/
-
+}
+}
