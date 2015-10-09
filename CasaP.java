d93394fe360ade2999c493c854f5cdfe2cdba5db
FernandoRojas07 <lfernandorojasf@gmail.com>
2015-10-08 19:30:59 -0500

    Update CasaP.java

--- CasaP.java
+++ CasaP.java
@@ -1,14 +1,26 @@
-class CasaP
-{
-   private DireccionP dir;
-   int indice, personas;
-   PersonaP []casas= new PersonaP[100];
-   public void CasasCap()
+class CasaP {
+   private DireccionP dir=new DireccionP();
+   private PersonaP[] casas;
+   int contperso=0,cont;
+   private PersonaP obj = new PersonaP();
+   
+   public void CasasCap(int cas)
    {
-      for(indice=0;indice<casas.length;indice++)
-      {
-         casas[indice].CapturaPersona();
+      casas= new PersonaP[cas];
+      for(cont=0;cont<cas;cont++)
+      {  
+         int noper;
+         noper=Teclado.LeeEntero("¿Cuántas personas hay en el hogar ["+(cont+1)+"]?");
+         casas[cont]=new PersonaP();
+         casas[cont].PersonaP(noper);
          dir.CapturaDireccion();
+         contperso=contperso+noper; 
       }
+   }
+   public int getIngreso(int a)
+   {
+      int hj;
+      hj=casas[a].getIng();
+      return hj;
    }
 }
