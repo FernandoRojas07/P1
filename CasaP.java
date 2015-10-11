9065486a123e62769c1bc8ff98b5a7350047f267
Mariooo2 <marioa_bros@hotmail.com>
2015-10-11 17:09:07 -0500

    Update CasaP.java

--- CasaP.java
+++ CasaP.java
@@ -1,5 +1,4 @@
 class CasaP {
-   private DireccionP dir=new DireccionP();
    private PersonaP[] casas;
    int contperso=0,cont,noper;
    private PersonaP obj = new PersonaP();
@@ -12,7 +11,6 @@ class CasaP {
          noper=Teclado.LeeEntero("¿Cuántas personas hay en el hogar ["+(cont+1)+"]?");
          casas[cont]=new PersonaP();
          casas[cont].PersonaP(noper);
-         dir.CapturaDireccion();
          contperso=contperso+noper; 
       }
    }
@@ -22,6 +20,12 @@ class CasaP {
       hj=casas[a].getIng();
       return hj;
    }
+   public int GetEstudios(int a)
+   {
+      int est;
+      est=casas[a].GetEst();
+      return est;
+   }
    public int getEdad(int a, int b)
    {
       int eda;
@@ -29,5 +33,20 @@ class CasaP {
          return casas[a].getEdV(noper);
       else
          return casas[a].getEdJ(noper);
+   }
+   public int getDireccion(int a){
+   int d;
+      d=casas[a].GetDir();
+      return d;
+   }
+   public int getFemenino(int a){
+   int f;
+   f=casas[a].GetFem();
+   return f;
+   }
+   public int GetIdioma(int a) {
+      int id;
+      id=casas[a].getIdi();
+      return id;
    }
 }
