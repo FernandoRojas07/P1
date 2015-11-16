278d59e777b1330e3d023d622c09da05f8ccd23f
FernandoRojas07 <lfernandorojasf@gmail.com>
2015-11-16 17:37:16 -0600

    Update ColaTLSE.java

--- ColaTLSE.java
+++ ColaTLSE.java
@@ -1,23 +1,36 @@
-class ColaTLSE {
-//Atributos
-private NodoT H, T;
-//Métodos
-public void Insertar(Trabajos dato){
-NodoT q=new NodoT(dato,null);
-if(T==null)
-H=q;
-else
-T.SetSig(q);
-T=q;
-}
-public NodoT Borrar(){
-NodoT aux=H;
-if(H==null)
-System.out.println("Error: no hay datos...");
-else if(H==T)
-H=T=null;
-else
-H=H.GetSig();
-return aux;
-}
+class ColaTLSE
+{
+   //Atributos
+   private NodoT H, T;
+   //Métodos
+   public void Insertar(Trabajos dato)
+   {
+      NodoT q=new NodoT(dato,null);
+      if(T==null)
+         H=q;
+      else
+         T.SetSig(q);
+      T=q;
+   }
+   public Trabajos Borrar()
+   {
+      NodoT aux=H;
+      if(H==null)
+         System.out.println("Error: no hay datos...");
+      else if(H==T)
+         H=T=null;
+      else
+         H=H.GetSig();
+      return aux.GetInfo();
+   }
+   public void Listar()
+   {
+      NodoT aux;
+      for(aux=H;aux!=null;aux=aux.GetSig())
+         System.out.println(aux.GetInfo());
+   }
+   public NodoT GetH()
+   {
+      return H;
+   }
 }
