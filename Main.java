df7677b44ed18c242667d6f2849adfb3d23c8539
FernandoRojas07 <lfernandorojasf@gmail.com>
2015-11-16 17:38:58 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,40 +1,42 @@
 class Main{
-
-	public static void main(String []args){
-        
+	public static void main(String []args)
+   {
+        int pro=0,pag=0,buffer=3;
+        ColaTLSE cs= new ColaTLSE();
         ColaCircularTLSE cc = new ColaCircularTLSE();
         System.out.println("\nBienvenido a este progrma\n");
-        int pro=Teclado.LeeEntero("\nNúmero de Trabajos: ");
-        int pag=Teclado.LeeEntero("\nNúmero de hojas a imprimir por turno: ");
-        
+        pro=Teclado.LeeEntero("\nNúmero de Trabajos: ");
+        pag=Teclado.LeeEntero("\nNúmero de hojas a imprimir por turno: ");
         System.out.println("Procesos iniciales creados:");
-        for(int i=0;i<pro;i++){
+        for(int i=0;i<pro;i++)
+        {
             Trabajos inicial = new Trabajos();
             inicial.ListarT();
-            cc.Insertar(inicial);
+            cs.Insertar(inicial);
         }
-        
-        System.out.println("\nSe han creador los procesos! Iniciaran los procesos...\n");
-            
-        while(!cc.EstaVacia()){
-        System.out.println("Se saca proceso: ");
-         Trabajos aux = cc.Borrar();
-         System.out.println(">>>>>>>>>>>");
-         aux.ListarT();
-         System.out.println(">>>>>>>>>>>");
-         //cc.listar();
-         if(aux.regresaNodo(pag)){
-            cc.Insertar(aux);
-         }else{
-            System.out.println("Proceso terminado: ");
+        System.out.println("\nSe han creador los procesos! \nIniciarán los procesos...\n");
+        for(int i=0;i<buffer;i++)
+        {
+            cc.Insertar(cs.Borrar());
+        }
+        while(!cc.ValidaVacio() || cs.GetH()==null)
+        {
+            System.out.println("Se saca proceso: ");
+            Trabajos aux = cc.Borrar();
+            System.out.println(">>>>>>>>>>>");
             aux.ListarT();
-         }
-         System.out.println("Inicia Listar");
-         cc.Listar();
-         System.out.println("Termina Listar");
-         
+            System.out.println(">>>>>>>>>>>");
+            //cc.listar();
+            if(aux.regresaNodo(pag))
+               cs.Insertar(aux);
+            else{
+               System.out.println("Proceso terminado: ");
+               aux.ListarT();
+            }
+            System.out.println("Inicia Listar");
+            cc.Listar();
+            System.out.println("Termina Listar");
+            cc.Insertar(cs.Borrar());  
         }
-    
 	}
-
 }
